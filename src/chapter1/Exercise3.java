1ec22a135d28cba23a1d1ca768fbcfcf7c470aa5
yokarak <54890038+yokarak@users.noreply.github.com>
2020-04-10 15:48:20 +0200

    Exercise 1.3

---
+++ src/chapter1/Exercise3.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+public class Exercise3 {
+
+    public static void main(String[] args) {
+        DaP();
+    }
+    public static void DaP() {
+        System.out.println("    J\n" +
+                           "J  aaa    v      vaa   \n" +
+                           "J  J  aa    v v    a a\n" +
+                           "J   aaaa     v     aaaa");
+    }
+}
