41de763e43cd0cf38b78feda698c1163db8aed4d
yokarak <54890038+yokarak@users.noreply.github.com>
2020-04-10 15:29:54 +0200

    exersise1.1

---
+++ src/chapter1/Exercise1.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+       hello();
+    }
+
+    public static void hello() {
+        System.out.println("Welcome to Java Learning Java Now\n" + "Programming is fun");
+    }
+}
