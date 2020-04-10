c8418e720300cbffc8052a983e8679c7b3a2b78e
yokarak <54890038+yokarak@users.noreply.github.com>
2020-04-10 16:02:09 +0200

    Exercise 1.6

---
+++ src/chapter1/Exercise6.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+public class Exercise6 {
+    public static void main(String[] args) {
+        sum();
+    }
+    public static void sum() {
+        int result = 1 +2+3+4+5+6+7+8+9+10;
+        System.out.println(result);
+    }
+}
