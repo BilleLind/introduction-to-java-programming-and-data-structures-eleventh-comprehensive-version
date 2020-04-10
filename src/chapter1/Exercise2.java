03af3f8d6c2b18039fb195646e2eccaa42d25109
yokarak <54890038+yokarak@users.noreply.github.com>
2020-04-10 15:31:55 +0200

    exercise 1.2

---
+++ src/chapter1/Exercise2.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        DFM();
+    }
+    public static void DFM() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("I love Java");
+        }
+    }
+}
